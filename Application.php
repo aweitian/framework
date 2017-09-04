6a537f6dd722a882bdf4aa2edc99e9c175f40df9
tiananwei <awei.tian@qq.com>
2017-09-04 17:56:56 +0800

    add composer files

---
+++ Application.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/9/4
+ * Time: 17:44
+ */
+namespace Tian\framework;
+use Tian;
+use tian\container;
+
+class Application extends container
+{
+
+}
