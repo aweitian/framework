fe346cf2271ea311896e23dc0c6e945b61bbc936
aw <531164479@qq.com>
2019-06-26 15:00:23 +0800

    fix composer.json

--- tests/web/public/index.php
+++ tests/web/public/index.php
@@ -11,17 +11,22 @@ require_once __DIR__ . '/../app/ctl.php';
 $app = new \Aw\Framework\Application(dirname(__DIR__));
 $request = new \Aw\Http\Request('/');
 $emitter = new \Aw\EventDispatcher();
-
+function l($c)
+{
+    static $log = array();
+    $log[] = $c;
+    return $log;
+}
 
 ///////////////  TEST EVENT ///////////////////////
 $emitter->addListener(\Aw\Framework\Event\Event::EVENT_REQUEST_CREATED, function (\Aw\Framework\Event\RequestCreated $event) {
-    print "EVENT_REQUEST_CREATED:" . $event->getRequest()->getPath() . " -- (0) -- \n";
+    l("EVENT_REQUEST_CREATED:" . $event->getRequest()->getPath() . " -- (0) -- \n");
 });
 $emitter->addListener(\Aw\Framework\Event\Event::EVENT_REQUEST_CREATED, function (\Aw\Framework\Event\RequestCreated $event) {
-    print "EVENT_REQUEST_CREATED:" . $event->getRequest()->getPath() . " -- (1) -- \n";
+    l("EVENT_REQUEST_CREATED:" . $event->getRequest()->getPath() . " -- (1) -- \n");
 });
 $emitter->addListener(\Aw\Framework\Event\Event::EVENT_ROUTE_MATCHED, function (\Aw\Framework\Event\RouterMatched $event) {
-    print "EVENT_ROUTE_MATCHED:" . $event->getRoute()->getName() . "\n";
+    l("EVENT_ROUTE_MATCHED:" . $event->getRoute()->getName() . "\n");
     $route = $event->getRoute();
     if ($route instanceof \Aw\Routing\Route) {
         $matcher = $route->getMatch();
@@ -29,22 +34,22 @@ $emitter->addListener(\Aw\Framework\Event\Event::EVENT_ROUTE_MATCHED, function (
             $parse = $matcher->matcher;
             if ($parse instanceof \Aw\Routing\Parse\Pmcai) {
                 $ctl = $parse->getControl();
-                var_dump($ctl);
+                log(var_export($ctl, true));
             }
         }
     }
 });
 $emitter->addListener(\Aw\Framework\Event\Event::EVENT_BEFORE_THROUGH_PRE_MIDDLEWARE, function (\Aw\Framework\Event\BeforePreMw $event) {
-    print "EVENT_BEFORE_THROUGH_PRE_MIDDLEWARE:" . $event->getRoute()->getName() . "\n";
+    l("EVENT_BEFORE_THROUGH_PRE_MIDDLEWARE:" . $event->getRoute()->getName() . "\n");
 });
 $emitter->addListener(\Aw\Framework\Event\Event::EVENT_AFTER_THROUGH_PRE_MIDDLEWARE, function (\Aw\Framework\Event\AfterPreMw $event) {
-    print "EVENT_AFTER_THROUGH_PRE_MIDDLEWARE:" . $event->getRoute()->getName() . "\n";
+    l("EVENT_AFTER_THROUGH_PRE_MIDDLEWARE:" . $event->getRoute()->getName() . "\n");
 });
 $emitter->addListener(\Aw\Framework\Event\Event::EVENT_BEFORE_INVOKE_ACTION, function (\Aw\Framework\Event\BeforeInvokeAction $event) {
-    print "EVENT_BEFORE_INVOKE_ACTION:" . $event->getRoute()->getName() . "\n";
+    l("EVENT_BEFORE_INVOKE_ACTION:" . $event->getRoute()->getName() . "\n");
 });
 $emitter->addListener(\Aw\Framework\Event\Event::EVENT_AFTER_INVOKE_ACTION, function (\Aw\Framework\Event\AfterInvokeAction $event) {
-    print "EVENT_AFTER_INVOKE_ACTION" . " -- (0) --\n";
+    l("EVENT_AFTER_INVOKE_ACTION" . " -- (0) --\n");
     $response = $event->getResponse();
     if ($response instanceof \Aw\Http\Response) {
         $response = $response->getContent();
@@ -52,7 +57,7 @@ $emitter->addListener(\Aw\Framework\Event\Event::EVENT_AFTER_INVOKE_ACTION, func
     $event->setResponse("hooked(" . $response . ")");
 });
 $emitter->addListener(\Aw\Framework\Event\Event::EVENT_AFTER_INVOKE_ACTION, function (\Aw\Framework\Event\AfterInvokeAction $event) {
-    print "EVENT_AFTER_INVOKE_ACTION" . " -- (1) --\n";
+    l("EVENT_AFTER_INVOKE_ACTION" . " -- (1) --\n");
     $response = $event->getResponse();
     if ($response instanceof \Aw\Http\Response) {
         $response = $response->getContent();
@@ -60,11 +65,11 @@ $emitter->addListener(\Aw\Framework\Event\Event::EVENT_AFTER_INVOKE_ACTION, func
     $event->setResponse($response . "again");
 });
 $emitter->addListener(\Aw\Framework\Event\Event::EVENT_BEFORE_SEND_RESPONSE, function (\Aw\Framework\Event\BeforeSendResponse $event) {
-    print "EVENT_BEFORE_SEND_RESPONSE" . "\n";;
+    l("EVENT_BEFORE_SEND_RESPONSE" . "\n");
     $event->setResponse($event->getResponse()->getContent() . " and again by send  response\n");
 });
 $emitter->addListener(\Aw\Framework\Event\Event::EVENT_AFTER_SEND_RESPONSE, function (\Aw\Framework\Event\AfterSendResponse $event) {
-    print "EVENT_AFTER_SEND_RESPONSE" . "\n";;
+    l("EVENT_AFTER_SEND_RESPONSE" . "\n");
 });
 ///////////////  TEST EVENT ///////////////////////
 
@@ -91,11 +96,11 @@ print "\n==================================================\n";
 
 $request = new \Aw\Http\Request('/not/found');
 $emitter->addListener(\Aw\Framework\Event\Event::EVENT_RESPONSE_404, function (\Aw\Framework\Event\Response404 $event) {
-    print "EVENT_RESPONSE_404" . "\n";
+    l("EVENT_RESPONSE_404" . "\n");
     $event->getResponse()->setContent("catch the 404");
 });
 $emitter->addListener(\Aw\Framework\Event\Event::EVENT_RESPONSE_500, function (\Aw\Framework\Event\Response500 $event) {
-    print "EVENT_RESPONSE_500" . "\n";
+    l("EVENT_RESPONSE_500" . "\n");
     $event->getResponse()->setContent("catch the 500");
 });
 $response = $kernel->handle($request);
@@ -108,7 +113,7 @@ $kernel->terminal();
  */
 $router = $app->make("router");
 
-print "\n==================================================\n";
+print  "\n==================================================\n";
 
 print $app->make('test')->v();
 
@@ -121,3 +126,4 @@ print "\n==================================================\n";
 
 print $app->make('ali')->ax();
 
+var_export(l(''));
